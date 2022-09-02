6eb28f08deb18529c9bc89f92d1abfe9fbc45668
RehmanSabuj <mushiur.sabuj@outlook.com>
2022-09-01 20:28:01 -0700

    Java Basic

---
+++ JavaBasicB2002/TestJava.java
@@ -0,0 +1,7 @@
+public class TestJava{
+public static void main(String [] args){
+
+System.out.println("Testing my java application");
+}
+
+}
---
+++ JavaBasicB2002/VariableDemo.java
@@ -0,0 +1,34 @@
+public class VariableDemo{
+public static void main(String [] args){
+
+int numberOfPhone = 6;
+int numberOfLaptops = 3;
+int totalGadagets = numberOfPhone + numberOfLaptops;
+
+    System.out.println("Total Gadagets I Have " + totalGadagets);
+
+    String topGradeStudent = "Liban";
+    char bottomGradeStudent = 'M';
+
+    //System.out.println("Print total students " + topG);
+byte numberOfBytes = 127;
+
+System.out.println("Number of Bytes " + numberOfBytes);
+
+boolean IsLibanAveryNaughtyBoy = false;
+System.out.println(IsLibanAveryNaughtyBoy);
+
+double valueOfBytes = 3.14;
+System.out.println("Print Value of Pie = " + valueOfBytes);
+add();
+
+}
+
+public static void add(){
+    int x = 4;
+    int y = 7;
+    int z = x + y;
+    System.out.println("Print sum of Z = " + z);
+}
+
+}
